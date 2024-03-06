10a771d237534b86194e9cfd8c8096330e787816
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2024-03-06 16:40:40 -0500

    Update test.js

--- Unit1_IntroToCoding_JavaScript/lab-001-hello_world/test.js
+++ Unit1_IntroToCoding_JavaScript/lab-001-hello_world/test.js
@@ -1,7 +1,6 @@
 // PUT YOUR helloWorld() FUNCTION RIGHT BELOW 
 
 function helloWorld(){
-
 }
 
 
@@ -15,14 +14,25 @@ function helloWorld(){
 // DO NOT TOUCH ANYTHING BELOW HERE
 //...please?
 function testHelloWorld(){
-    if (helloWorld() == ("Hello World"))
-        console.log("You did it! Congrats on finishing your first lab! You can move on to the next!")
-    else if (helloWorld().include("Hello World"))
-        console.log("You're so close! You have extra characters in the way, maybe extra spaces")
-    else if (helloWorld().include("ello") || helloWorld().include("orld"))
-        console.log("You're close, remember to capitalize the H and W in Hello World")
-    else
-        console.log(`Not quite, your method returns... \n${helloWorld()}\nand we are looking to get\nHello World`)
+    try {
+        console.log("\n")
+        console.log("Your function returns... ", helloWorld())
+        if (helloWorld() == ("Hello World"))
+            console.log("You did it! Congrats on finishing your first lab! You can move on to the next!")
+        else if (helloWorld().include("Hello World"))
+            console.log("You're so close! You have extra characters in the way, maybe extra spaces")
+        else if (helloWorld().include("ello") || helloWorld().include("orld"))
+            console.log("You're close, remember to capitalize the H and W in Hello World")
+        else
+            console.log(`Not quite, your method returns... \n${helloWorld()}\nand we are looking to get\nHello World`)
+    }
+    catch(err){ 
+        if (err.toString().includes("Cannot read properties of undefined")){
+            console.log("Your function has no return value! Make sure to have it `return` something!")
+        }
+    }
+    console.log("\n")
+    
 }
 
 testHelloWorld()
